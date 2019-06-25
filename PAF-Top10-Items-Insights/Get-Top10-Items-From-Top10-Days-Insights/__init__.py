1d6197f754137a1e4ee17e0fdf6784c73656f9dc
Dmitry Katson <dmitry.katson@gmail.com>
2019-06-25 16:14:21 +0300

    bugfix

--- PAF-Top10-Items-Insights/Get-Top10-Items-From-Top10-Days-Insights/__init__.py
+++ PAF-Top10-Items-Insights/Get-Top10-Items-From-Top10-Days-Insights/__init__.py
@@ -3,6 +3,7 @@ import logging
 import azure.functions as func
 
 import pandas as pd
+import json
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
